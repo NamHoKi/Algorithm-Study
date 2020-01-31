a14168f80c63b7ff0fef1d38033034babe32ae1a
Ho ki Nam <48282708+NamHoKi@users.noreply.github.com>
2020-01-31 23:10:55 +0900

    단어 변환 first commit

---
+++ Programmers cording test practice/깊이,너비 우선 탐색/Problem3.py
@@ -0,0 +1,10 @@
+def solution(begin, target, words):
+    answer = 0
+    def DFS(target,words,cnt):
+        if not (target in words):
+            return 0
+        if target in words:
+            return cnt + 1
+        for i in range(len(words)):
+
+    return answer
