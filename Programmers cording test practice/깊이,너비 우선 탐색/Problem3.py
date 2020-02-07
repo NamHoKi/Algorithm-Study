dfd9eecb69a9e66bcbb85469232ba0740f8731fb
Ho ki Nam <48282708+NamHoKi@users.noreply.github.com>
2020-02-07 22:30:29 +0900

    단어 변환 : 테스트케이스 3 안됨

--- Programmers cording test practice/깊이,너비 우선 탐색/Problem3.py
+++ Programmers cording test practice/깊이,너비 우선 탐색/Problem3.py
@@ -1,12 +1,25 @@
 def solution(begin, target, words):
-    answer = 0
-    def DFS(target,words,cnt):
-        if not (target in words):
-            return 0
-        if target in words:
-            return cnt + 1
+    def DFS(begin , target , words, count, result_array):
+        if not(target in words):
+            return [0]
+        if begin == target:
+            return [count]
+        check = False
         for i in range(len(words)):
-
-    return answer
-
-#####
+            diff_cnt = 0
+            for j in range(len(words[0])):
+                if words[i][j] != begin[j]:
+                    diff_cnt += 1
+            if diff_cnt == 1:
+                check = True
+                print(words[i:] + words[:i])
+                result_array += DFS(words[i], target, words[i:], count+1, result_array)
+        if not check:
+            return result_array + [-1]
+        return result_array
+    result = DFS(begin, target, words, 0 , [])
+    result.sort()
+    for i in range(len(result)):
+        if result[i] >= 0:
+            return result[i]
+    return 0
