6fb611d700a433a64a86263ed89ce341a5285ad1
Ho ki Nam <48282708+NamHoKi@users.noreply.github.com>
2020-02-16 22:42:04 +0900

    H-Index

--- Programmers cording test practice/정렬/Problem3.py
+++ Programmers cording test practice/정렬/Problem3.py
@@ -1,7 +1,10 @@
 def solution(citations):
     citations.sort()
-    count, h_index , n = 0, 0, len(citations)    
-    for c in citations:
-        
-        
-    return h_index
+    count, h , n = 0, 0, len(citations)    
+    for i in range(len(citations)-1, -1 ,-1):
+        if i != 0 and citations[i-1] != citations[i] and count >= citations[i]:
+            return citations[i]
+        count += 1
+    return h
+
+## 안되는 케이스가 있음
