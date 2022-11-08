82b102ccc96d30a5aa1b9d79f07be741cabf6a77
Ho ki Nam <48282708+NamHoKi@users.noreply.github.com>
2022-11-08 22:16:50 +0900

    Create 모스부호120838.py

---
+++ Programmers cording test practice/MS_AI_SCHOOL_TIL/모스부호120838.py
@@ -0,0 +1,20 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120838 <- 프로그래머스 120838번: 모스부호(1)
+
+
+def solution(letter):
+    
+    morse = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    }
+    
+    letter = letter.split()
+    answer = ''
+    
+    for c in letter:
+        answer += morse[c]
+    
+    return answer
