e28846873765766df0dcfe449b1a408734c21317
Ho ki Nam <48282708+NamHoKi@users.noreply.github.com>
2022-11-19 22:54:19 +0900

    Update cur_code.py

--- 프로그래머스/cur_code.py
+++ 프로그래머스/cur_code.py
@@ -2,19 +2,25 @@ from itertools import permutations
 import math
 
 def is_prime_number(x):
-    for i in range(2, int(math.sqrt(x)) + 1):
+    if x <= 1:
+        return 0
+    for i in range(2, int(math.sqrt(x)) + 2):
         if x % i == 0:
-            return False
-    return True
+            return 0
+    return 1
 
 def solution(numbers):
-    answer = 0
-    permutations_list = list(permutations(list(numbers), len(numbers)))
-    num_list = []
-    for t in permutations_list:
-        num_list.append(int(''.join(list(t))))
-    num_list = set(num_list)
-    for number in numbers:
-        if is_prime_number(int(number)): answer += 1
+    answer, num_list = 0, list(numbers)
+        
+    for i in range(2, len(numbers) + 1):
+        permutations_list = list(permutations(list(numbers), i))
+        for num_tuple in permutations_list:     # 각 자리수 리스트 -> 문자열 -> 정수
+            num_list += [int(''.join(list(num_tuple)))]
+            
+    num_list = list(set(num_list))              # 중복 제거
+
+    for number in num_list:                     # 소수라면 answer + 1
+        answer += is_prime_number(int(number))
 
     return answer
+    
