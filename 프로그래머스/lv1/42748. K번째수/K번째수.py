5c996bf6c8b2ede7f263246c9bfbbc72c39f5a29
Ho ki Nam <48282708+NamHoKi@users.noreply.github.com>
2023-03-24 00:15:08 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,7 +1,10 @@
 def solution(array, commands):
     answer = []
     for command in commands:
-        find_array = array[command[0] - 1:command[1]]
-        find_array.sort()
-        answer.append(find_array[command[2]-1])
+        i, j, k = command
+        i, k = i - 1, k - 1
+        a = array[i:j]
+        a.sort()
+        answer.append(a[k])
+        
     return answer
