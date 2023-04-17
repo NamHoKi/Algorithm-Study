7e50e52d867bc26932aebdb3402fefbaa6e3a0b1
Ho ki Nam <48282708+NamHoKi@users.noreply.github.com>
2023-04-18 01:11:06 +0900

    [level 2] Title: 소수 찾기, Time: 0.89 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,37 @@
+def is_prime_number(x):
+    if x <= 1:
+        return 0
+    for i in range(2, x):
+        if x % i == 0:
+            return 0
+    return 1
+
+
+def recursion(cur_number, numbers, index) :
+    global number_list
+    
+    if cur_number != '' :
+        number_list.append(cur_number)
+        del numbers[index]
+    
+    if len(numbers) == 0 :
+        return
+    else :
+        for i in range(len(numbers)) :
+            recursion(cur_number + numbers[i], numbers[:], i)
+
+
+def solution(numbers):
+    answer = 0
+    global number_list
+    number_list = []
+    numbers = list(numbers)
+    
+    recursion('', numbers, 0)
+    number_set = set(list(map(int, number_list)))
+    
+    for number in number_set :
+        answer +=  is_prime_number(number)
+
+    return answer
+    
